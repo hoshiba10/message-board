28728f6e1ad6d473b74d2afaf6ca32abcb688d2e
hoshiba10 <j_come_true@yahoo.co.jp>
2016-02-17 14:07:39 +0000

    add validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20 } , presence: true
     
     validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+    
+    validates :age , presence: true, numericality: true
 end
